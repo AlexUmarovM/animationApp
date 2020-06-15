56a3fb8002a258b59ae12ca836f3c2b49b133228
Alex Umarov <umarov.iosdev@gmail.com>
2020-06-15 20:23:50 +0300

    Initial Commit

---
+++ animationApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  animationApp
+//
+//  Created by Александр Умаров on 15.06.2020.
+//  Copyright © 2020 Александр Умаров. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
